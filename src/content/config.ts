5b7f942beee82117a9c42a154777d4bff08c54a9
Mikaël Sévigny <sevignator@hotmail.com>
2024-04-11 15:22:33 -0400

    Make blog images optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,11 +6,12 @@ const postsCollection = defineCollection({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
-    author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
     tags: z.array(z.string()),
   }),
 });
